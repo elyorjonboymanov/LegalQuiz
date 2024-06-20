244568bc5db1c467ecdfddb36dd8aa6e0f62cc97
elyorjonboymanov <boymanovelyorjon@gmail.com>
2024-06-20 08:41:10 +0500

    E

--- LegalQuiz2/obj/Debug/net5.0-windows/App.g.cs
+++ LegalQuiz2/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace LegalQuiz2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.18.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace LegalQuiz2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.18.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
         public static void Main() {
             LegalQuiz2.App app = new LegalQuiz2.App();
             app.InitializeComponent();
